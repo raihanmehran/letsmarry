5e6e6b8cfac89e55aebf01518e8aecff89a78cfa
Mehran <raihan.mehran@hotmail.com>
2023-01-04 18:46:31 +0100

    Simple login implemented

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -21,6 +21,7 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
             if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
@@ -31,6 +32,25 @@ namespace API.Controllers
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u =>
+                u.UserName == loginDto.Username);
+
+            if(user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0; i<computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+
             return user;
         }
 
