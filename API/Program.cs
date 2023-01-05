07d198fc146853421518e61cff05c2eaff8d88a0
Mehran <raihan.mehran@hotmail.com>
2023-01-05 10:12:15 +0100

    Auth Middleware + Extensions Methods

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,23 @@
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdenttyServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder
+   .AllowAnyHeader()
+   .AllowAnyMethod()
+   .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
