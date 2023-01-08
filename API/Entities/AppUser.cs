247c3bbb6d11965b3eef7eefb2ff0237c9326134
Mehran <raihan.mehran@hotmail.com>
2023-01-08 13:33:58 +0100

    Edit Member info done in client-side

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -16,6 +16,7 @@ namespace API.Entities
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
         public string Introduction { get; set; }
+        public string LookingFor { get; set; }
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PreventUnsavedChangesGuard
+  implements CanDeactivate<MemberEditComponent>
+{
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.eidtForm?.dirty) {
+      return confirm(
+        'Are you sure want to continue? Any unsaved changes will be lost!'
+      );
+    }
+    return true;
+  }
+}
