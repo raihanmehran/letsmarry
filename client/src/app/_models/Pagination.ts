67e118a31108e0dcc39f9b035423bc5915ba41ea
Mehran <raihan.mehran@hotmail.com>
2023-01-12 11:33:56 +0100

    Pagination Just Available in member-list component

---
+++ client/src/app/_models/Pagination.ts
@@ -0,0 +1,10 @@
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPages: number;
+}
+export class PaginationResult<T> {
+  result?: T;
+  pagination?: Pagination;
+}
