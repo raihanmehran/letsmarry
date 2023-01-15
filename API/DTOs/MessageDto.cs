cd492070f72466f7c5b8b9a1c29c65ab98d578d9
Mehran <raihan.mehran@hotmail.com>
2023-01-15 19:00:57 +0100

    MessageRepository Added

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -1,19 +1,19 @@
+using API.Entities;
 
-namespace API.Entities
+namespace API.DTOs
 {
-    public class Message
+    public class MessageDto
     {
         public int Id { get; set; }
         public int SenderId { get; set; }
         public string SenderUsername { get; set; }
-        public AppUser Sender { get; set; }
+        public string SenderPhotoUrl { get; set; }
+
         public int RecipientId { get; set; }
         public string RecipientUsername { get; set; }
-        public AppUser Recipient { get; set; }
+        public string RecipientPhotoUrl { get; set; }
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
-        public DateTime MessageSent { get; set; } = DateTime.UtcNow;
-        public bool SenderDeleted { get; set; }
-        public bool RecipientDeleted { get; set; }
+        public DateTime MessageSent { get; set; }
     }
 }
---
+++ API/Data/Migrations/20230115175325_MessageEntityAdded2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageEntityAdded2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientUsername",
+                table: "Messages",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecipientUsername",
+                table: "Messages");
+        }
+    }
+}
