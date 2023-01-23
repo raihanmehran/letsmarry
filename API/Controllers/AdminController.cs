22b8fc51d5ce1db8af48e5ea03255209395562b2
Mehran <raihan.mehran@hotmail.com>
2023-01-23 21:15:21 +0100

    Editing roles added in AdminController

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -32,6 +32,32 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+
+            var selectedRoles = roles.Split(",").ToArray();
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
+
         [Authorize(Policy = "ModeratorPhotoRole")]
         [HttpGet("photos-to-moderate")]
         public ActionResult GetPhotoForModeration()
