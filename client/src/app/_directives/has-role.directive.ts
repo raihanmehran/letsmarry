11bd6376ed68335d45c5e6c4c080dcb2b2fbc6a7
Mehran <raihan.mehran@hotmail.com>
2023-01-24 16:46:34 +0100

    Angular Custom Directive added for admin

---
+++ client/src/app/_directives/has-role.directive.ts
@@ -0,0 +1,37 @@
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+import { take } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Directive({
+  selector: '[appHasRole]',
+})
+export class HasRoleDirective implements OnInit {
+  @Input() appHasRole: string[] = [];
+  user: User = {} as User;
+
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private templateRef: TemplateRef<any>,
+    private accountSerivce: AccountService
+  ) {
+    this.accountSerivce.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) this.user = user;
+      },
+    });
+  }
+  ngOnInit(): void {
+    if (this.user.roles.some((r) => this.appHasRole.includes(r))) {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainerRef.clear();
+    }
+  }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
     TextInputComponent,
     DatePickerComponent,
     MemberMessagesComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    HasRoleDirective
   ],
   imports: [
     BrowserAnimationsModule,
